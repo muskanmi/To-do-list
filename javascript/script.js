bd773e62b2e91430ef52a42f9268bd958d72b63a
Muskan Mittal <muskan.mittal@w3villa.com>
2022-12-15 17:11:04 +0530

    to do list source code added

---
+++ javascript/script.js
@@ -0,0 +1,78 @@
+
+let input = document.querySelector('.entered-list');
+let addBtn = document.querySelector('.add-list');
+let tasks = document.querySelector('.tasks');
+
+//add btn disabled
+
+input.addEventListener('keyup', () => {
+    if(input.value.trim() != 0) {
+        addBtn.classList.add('active');
+    }
+    else{
+        addBtn.classList.remove('active');
+    }
+})
+
+//add new item to list
+addBtn.addEventListener('click', () => {
+    if (input.value.trim() != 0 ){
+        let newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = `
+        <p> ${input.value} </p>
+        <div class="item-btn">
+            <i class="fa-solid fa-pen-to-square"></i>
+            <i class="fa-solid fa-xmark"></i>
+            <i class="fa-solid fa-pen"></i>
+        </div>
+        `;
+
+        tasks.appendChild(newItem);
+        input.value = '';
+    } else {
+        alert('Please enter a task!')
+    }
+})
+
+//delete item from list
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-xmark')) {
+        e.target.parentElement.parentElement.remove();
+    }
+})
+
+//mark item as completed
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-pen-to-square')) {
+        e.target.parentElement.parentElement.classList.toggle('completed');
+    }
+})
+
+//rename existing item in a list
+tasks.addEventListener('click', (e) => {
+    if (e.target.classList.contains('fa-pen')) {
+        input.value = e.target.parentElement.previousElementSibling.innerHTML;
+		e.target.parentElement.parentElement.remove();
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
